e8449d377f584b64bc28449b25e0a9bf94c00df8
Tahdi Islam <tshdi60@gmail.com>
2024-08-12 10:54:25 +0600

    complete homepage flower slider

--- components/flowerLimit.jsx
+++ components/flowerLimit.jsx
@@ -5,23 +5,17 @@ import dummyImage from "@/public/flower_1.png";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import axios from "axios";
-// import Swiffy Slider JS
-import { swiffyslider } from "swiffy-slider";
-
-// import Swiffy Slider CSS
-import "swiffy-slider/css";
-
-if (typeof window !== "undefined") {
-    window.swiffyslider = swiffyslider;
-
-    window.addEventListener("load", () => {
-      window.swiffyslider.init();
-    });
-}
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export default function FlowersLimit() {
   const [flowers, setFlowers] = useState(null);
-  
+
   const handleLoadFlowers = () => {
     axios
       .get(`${process.env.NEXT_PUBLIC_BACKEDN_URL_PROD}/flower/list/?limit=7`)
@@ -39,21 +33,31 @@ export default function FlowersLimit() {
   return (
     <div className="w-full p-4">
       <h1 className="text-3xl font-bold p-4">All Flowers</h1>
-      <div class="swiffy-slider slider-item-show4 slider-item-show2-sm slider-nav-round slider-nav-scrollbar slider-nav-animation">
-        <ul class="slider-container">
+      <Carousel
+        opts={{
+          align: "start",
+        }}
+        className="w-[95%] mx-auto"
+      >
+        <CarouselContent>
           {flowers?.results?.map((flower) => (
-            <li key={flower?.id}>
+            <CarouselItem
+              key={flower?.id}
+              className="md:basis-1/2 lg:basis-1/3 xl:basis-1/4 cursor-move select-none"
+            >
               <div
                 id={`slide${flower?.id}`}
-                className="max-w-[350px] space-y-4 rounded-lg bg-white p-6 shadow-lg md:w-[350px] dark:bg-[#18181B]"
+                className="max-w-[350px] space-y-4 rounded-lg bg-white p-6 shadow-lg md:w-[340px] dark:bg-[#18181B]"
               >
-                <Image
-                  width={200}
-                  height={200}
-                  className="h-[275px] w-[350px] rounded-lg object-cover"
-                  src={dummyImage}
-                  alt={flower?.title}
-                />
+                <picture className="rounded-lg overflow-hidden block">
+                  <Image
+                    width={200}
+                    height={200}
+                    className="h-[275px] w-[350px] rounded-lg object-cover hover:scale-125 ease-linear duration-200"
+                    src={dummyImage}
+                    alt={flower?.title}
+                  />
+                </picture>
                 <div className="grid gap-2">
                   <h1 className="text-lg font-semibold ">
                     {flower?.title?.length <= 30
@@ -73,25 +77,12 @@ export default function FlowersLimit() {
                   </Link>
                 </div>
               </div>
-            </li>
+            </CarouselItem>
           ))}
-        </ul>
-
-        <button type="button" class="slider-nav"></button>
-        <button type="button" class="slider-nav slider-nav-next"></button>
-
-        <ul class="slider-indicators">
-          <li class=""></li>
-          <li class=""></li>
-          <li class=""></li>
-          <li class=""></li>
-          <li class=""></li>
-          <li class=""></li>
-          <li class=""></li>
-          <li class=""></li>
-          <li class="active"></li>
-        </ul>
-      </div>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   );
 }
