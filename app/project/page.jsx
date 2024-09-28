b4125a9e72a38c1c7eb81ede8dedf0ca92c12d51
Tahdi Islam <tshdi60@gmail.com>
2024-09-29 00:19:11 +0600

    [Done] complete project create and all project list page

--- app/project/page.jsx
+++ app/project/page.jsx
@@ -14,13 +14,17 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useUserContext } from "@/contexts/userContext";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Profile({ params }) {
   const { user } = useUserContext();
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [end_date, setEnd_date] = useState("");
+  const [projects, setProjects] = useState([]);
+  console.log("🚀 ~ Profile ~ projects:", projects)
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const handleSubmit = async () => {
     const formData = {
@@ -29,23 +33,43 @@ export default function Profile({ params }) {
       end_date: end_date,
       created_by: user?.user?.id,
     };
-    console.log("🚀 ~ handleSubmit ~ formData:", formData);
     try {
       const response = await axios.post(
         `${process.env.NEXT_PUBLIC_BACKEND_URL_PROD}/project/list/`,
         formData
       );
+      setProjects((prev) => [...prev, response.data]);
       console.log("Project Created:", response.data);
     } catch (error) {
       console.error("Error creating project:", error);
     }
   };
+  useEffect(() => {
+    if (!user?.user?.id) return;
+    const fetchProjects = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL_PROD}/project/list/?user_id=${user?.user?.id}`
+        );
+        console.log("🚀 ~ fetchProjects ~ response:", response?.data?.results)
+        setProjects(response?.data?.results);
+        setLoading(false);
+      } catch (err) {
+        setError(err);
+        setLoading(false);
+      }
+    };
+
+    fetchProjects();
+  }, [user?.user?.id]);
+  if (loading) return <p>Loading...</p>;
+  //   if (error) return <p>Error loading projects.</p>;
   return (
     <div className="p-10 flex flex-col items-start gap-4">
       <h1 className="text-2xl font-semibold">Create a new project</h1>
       <Dialog>
         <DialogTrigger asChild>
-          <button className="text-xl font-semibold py-2 px-6 rounded-xl bg-blue-600 hover:bg-blue-700 text-white transition ease-in-out duration-500 mt-4">
+          <button className="text-xl font-semibold py-2 px-6 rounded-md bg-blue-600 hover:bg-blue-700 text-white transition ease-in-out duration-500 mt-4">
             Create new
           </button>
         </DialogTrigger>
@@ -98,18 +122,29 @@ export default function Profile({ params }) {
         </DialogContent>
       </Dialog>
       <h1 className="text-2xl font-semibold">Your projects</h1>
-      <div>
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md p-6">
-          <h2 class="text-xl font-semibold text-gray-800 mb-2">
-            Website Redesign
-          </h2>
-          <p class="text-gray-600 mb-4">
-            Redesign the company website with a modern UI/UX.
-          </p>
-          <div class="flex justify-between items-center">
-            <span class="text-sm text-gray-500">End Date: 2024-12-15</span>
-            <span class="text-sm text-gray-500">Created by User #1</span>
-          </div>
+      <div class="container mx-auto p-4">
+        <div class="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {projects.length ? (
+            projects?.map((project) => (
+              <div
+                key={project.id}
+                class="bg-white border border-gray-200 rounded-lg shadow-md p-6"
+              >
+                <h2 class="text-xl font-semibold text-gray-800 mb-2">
+                  {project.name}
+                </h2>
+                <p class="text-gray-600 mb-4">{project.description}</p>
+                <div class="flex justify-between items-center">
+                  <span class="text-sm text-gray-500">
+                    End Date: {project.end_date}
+                  </span>
+                  <span class="text-sm text-gray-500">Created by User #1</span>
+                </div>
+              </div>
+            ))
+          ) : (
+            <h1 className="text-2xl font-semibold">No projects found</h1>
+          )}
         </div>
       </div>
     </div>
